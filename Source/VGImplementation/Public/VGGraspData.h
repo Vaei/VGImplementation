d9c8f36e781522bb1f4257a5fcef4d1d8ceaa957
Jared Taylor <jaredtaylor.99@gmail.com>
2025-12-22 11:01:05 +1300

    Add toggle option hint + hint size scalar

--- Source/VGImplementation/Public/VGGraspData.h
+++ Source/VGImplementation/Public/VGGraspData.h
@@ -15,9 +15,19 @@ class VGIMPLEMENTATION_API UVGGraspData : public UGraspData
 	GENERATED_BODY()
 	
 public:
+	/** Text to show in the interaction prompt when looking at this graspable object */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Grasp)
 	FText InteractionPromptText = FText::FromString("Interact");
 	
+	/** Description text to show in the interaction prompt when looking at this graspable object and within interactable range */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Grasp)
 	FText InteractionPromptDescription = FText::GetEmpty();
+	
+	/** Whether to show an interaction option hint when looking at a different graspable object, but within highlight range of this one */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Grasp, meta=(UIMin="0.1", ClampMin="0.001", UIMax="2", Delta="0.05", ForceUnits="x"))
+	bool bAllowOptionHint = true;
+	
+	/** Size scalar for the interaction option hint, utilized when looking at a different graspable object, but within highlight range of this one */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Grasp, meta=(UIMin="0.1", UIMax="2", Delta="0.05", ForceUnits="x", EditCondition="bAllowOptionHint", EditConditionHides))
+	float OptionHintSizeScalar = 1.f;
 };
